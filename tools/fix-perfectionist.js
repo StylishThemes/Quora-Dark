79ed49d9c010667161ecefbb6e2ca85a33a07637
Rob Garrison <wowmotty@gmail.com>
2018-08-18 20:13:07 -0500

    Meta: Remove extra carriage returns between definitions

--- tools/fix-perfectionist.js
+++ tools/fix-perfectionist.js
@@ -7,15 +7,18 @@ const pkg = require("../package.json");
 
 const fileName = path.join(__dirname, "..", pkg.main);
 
-// Perfectionist adds comments to the end of the previous line...
-// }/* comment */ => }\n\n  /* comment */
-function adjustComments(css) {
-  return css.replace(/}\/\*(([\s\S])+?)\*\/\s*/g, "}\n\n  /*$1*/\n  ");
+function cleanup(css) {
+  return css
+    // Perfectionist adds comments to the end of the previous line...
+    // }/* comment */ => }\n\n  /* comment */
+    .replace(/}\/\*(([\s\S])+?)\*\/\s*/g, "}\n\n  /*$1*/\n  ")
+    // Remove extra carriage returns between definitions
+    .replace(/\n+/g, "\n");
 }
 
 async function postPerfectionist() {
   const css = await fs.readFile(fileName, "utf8");
-  await fs.writeFile(fileName, adjustComments(css));
+  await fs.writeFile(fileName, cleanup(css));
   console.log("\x1b[32m%s\x1b[0m", `${pkg.title} usercss cleanup completed`);
 }
 
